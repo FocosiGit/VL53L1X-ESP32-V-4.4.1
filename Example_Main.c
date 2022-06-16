f5969152f838a83e53b42d0d0f2ea54fd9f275f6
FocosiGit <60970261+FocosiGit@users.noreply.github.com>
2022-06-15 23:11:01 -0300

    Update Example_Main.c

--- Example_Main.c
+++ Example_Main.c
@@ -30,8 +30,8 @@
  * Wiring :  connect SCL cable in  GPIO 15 (D15)
  * 			 connect SDA cable in  GPIO 16 (RX2)
  */
-#define I2C_MASTER_SCL_IO               15
-#define I2C_MASTER_SDA_IO               16
+#define I2C_MASTER_SCL_IO               21
+#define I2C_MASTER_SDA_IO               22
 #define I2C_MASTER_NUM                  I2C_NUM_0
 #define I2C_MASTER_TX_BUF_DISABLE       0
 #define I2C_MASTER_RX_BUF_DISABLE       0
